ac5ee19c3a20d2664a951dae8c72d0b30bc79bbb
FrogMerk <edwincrooks@live.com>
2026-05-16 07:39:24 +1000

    moved graph down 8px

--- boards/shields/nice_oled/widgets/wpm.c
+++ boards/shields/nice_oled/widgets/wpm.c
@@ -222,7 +222,7 @@ static void draw_grid(lv_obj_t *canvas) {
     lv_draw_img_dsc_t img_dsc;
     lv_draw_img_dsc_init(&img_dsc);
 
-    lv_canvas_draw_img(canvas, 0, 67, &grid, &img_dsc);
+    lv_canvas_draw_img(canvas, 0, 75, &grid, &img_dsc);
 }
 
 static void draw_graph(lv_obj_t *canvas, const struct status_state *state) {
@@ -245,7 +245,7 @@ static void draw_graph(lv_obj_t *canvas, const struct status_state *state) {
 
         // modificar aqui par la posicion de la grafica
         points[i].x = i * 3.44;
-        points[i].y = 99 - (value * 30 / max);
+        points[i].y = 107 - (value * 30 / max);
         // points[i].y = 132 - (value * 32 / max);
     }
 #else
@@ -268,7 +268,7 @@ static void draw_graph(lv_obj_t *canvas, const struct status_state *state) {
 
     for (int i = 0; i < 10; i++) {
         points[i].x = i * 3.44;
-        points[i].y = 99 - (state->wpm[i] - min) * 30 / range;
+        points[i].y = 107 - (state->wpm[i] - min) * 30 / range;
     }
 #endif
 
